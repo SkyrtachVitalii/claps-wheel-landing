a1a30c014c76847ecc770cc4a3b4937663ecd9a3
Vitalii Skyrtach <vitalijskirtac@gmail.com>
2025-01-28 16:08:39 +0200

    stable version before updating working with MongoDB

--- proxy-server/modelsMongoDB/findMongoDB.js
+++ proxy-server/modelsMongoDB/findMongoDB.js
@@ -10,6 +10,12 @@ async function checkUserBonus(incomingName) {
     },
   });
 
+  const resBody = {
+    name: incomingName,
+    bonus: null,
+    date: null,
+  };
+
   function checkEqualsDay(dateOne, dateTwo) {
     // console.log("--------------");
     // console.log("incomingDate - ", dateOne);
@@ -20,10 +26,7 @@ async function checkUserBonus(incomingName) {
       dateOne.getMonth() === dateTwo.getMonth() &&
       dateOne.getDate() === dateTwo.getDate();
 
-    const existingBonus = areDatesEqual
-      ? "userExist_bonusExist"
-      : "userExist_bonusNotExist";
-
+    const existingBonus = areDatesEqual ? true : false;
     return existingBonus;
   }
 
@@ -39,27 +42,24 @@ async function checkUserBonus(incomingName) {
       name: incomingName,
     });
 
-    const allUsersWithBonus = await bonuses.find({
-        name: incomingName,
-    }).sort({date:1}).toArray();
-    console.log(allUsersWithBonus);
-    // console.log("--------------");
-    // console.log("incomingName - ", incomingName);
-    // console.log("foundName - ", oneUser);
-    // console.log("--------------");
-    
+    console.log(oneUser);
+
     let incomingUserDate;
     oneUser === null || oneUser === undefined
       ? (incomingUserDate = null)
       : (incomingUserDate = oneUser.date);
 
     if (incomingUserDate != null || incomingUserDate != undefined) {
-      return {
+      const extUserData = {
+        ...oneUser,
         bonusExist: checkEqualsDay(today, incomingUserDate),
-        incomingUserDate,
       };
+      return extUserData;
     } else {
-      return false;
+      return {
+        userExist: false,
+        bonusExist: false,
+      };
     }
   } finally {
     await client.close();
@@ -85,8 +85,8 @@ async function addNewUserBonus(name, randomBonus) {
     const db = client.db(process.env.MONGO_DB);
     const collection = db.collection(process.env.MONGO_COLLECTION);
     const result = await collection.insertOne(resBody);
-    console.log("Документ створено з ID:", result.insertedId);
-    
+    // console.log("Документ створено з ID:", result.insertedId);
+
     return resBody;
   } catch (error) {
     console.error("Помилка запису в базу даних:", error);
@@ -94,42 +94,41 @@ async function addNewUserBonus(name, randomBonus) {
   }
 }
 
-async function updateUserBonus (name, randomBonus) {
-    const resBody = {
-        name,
-        bonus: randomBonus,
-        date: new Date(),
-      };
-    const client = new MongoClient(process.env.MONGO_URI, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
-      const filter = { name: name };
-      const updateDoc = {
-        $set: {
-            bonus: randomBonus,
-            date: resBody.date
-        }
-      }
-      const options = { upsert: true };
+async function updateUserBonus(name, randomBonus) {
+  const resBody = {
+    name,
+    bonus: randomBonus,
+    date: new Date(),
+  };
+  const client = new MongoClient(process.env.MONGO_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  const filter = { name: name };
+  const updateDoc = {
+    $set: {
+      bonus: randomBonus,
+      date: resBody.date,
+    },
+  };
+  const options = { upsert: true };
 
-      try {
-        await client.connect();
-        const db = client.db(process.env.MONGO_DB);
-        const collection = db.collection(process.env.MONGO_COLLECTION);
-        const result = await collection.updateOne(filter, updateDoc, options);
-        console.log("Документ оновлено з ID:", result);
-        
-        return resBody;
-      } catch (error) {
-        console.error("Помилка запису в базу даних:", error);
-        res.status(500).json({ error: "Не вдалося зберегти дані в базу" });
-      }
-    console.log("updateUserBonus", name);
-    
+  try {
+    await client.connect();
+    const db = client.db(process.env.MONGO_DB);
+    const collection = db.collection(process.env.MONGO_COLLECTION);
+    const result = await collection.updateOne(filter, updateDoc, options);
+    // console.log("Документ оновлено з ID:", result);
+
+    return resBody;
+  } catch (error) {
+    console.error("Помилка запису в базу даних:", error);
+    res.status(500).json({ error: "Не вдалося зберегти дані в базу" });
+  }
+  // console.log("updateUserBonus", name);
 }
 
 module.exports = { checkUserBonus, addNewUserBonus, updateUserBonus };
--- proxy-server/server.js
+++ proxy-server/server.js
@@ -7,36 +7,38 @@ const { checkUserBonus, addNewUserBonus, updateUserBonus } = require("./modelsMo
 const getRandomBonus = require("./random")
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4040;
+
+
+// Налаштовуємо CORS middleware для дозволу лише з конкретного домену
+const allowedOrigins = ['https://claps.com', 'https://claps.bet', 'https://claps.guru', 'https://prev.cc2.prod.bid'];  // Дозволені домени
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // Дозволяємо запити з дозволених доменів або з локального хоста
+      callback(null, true);
+    } else {
+      // Блокуємо запити з інших доменів
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['POST'],  // Дозволяємо лише POST запити
+  allowedHeaders: ['Content-Type'],  // Дозволяємо лише Content-Type заголовок
+};
+
+
 
 // Додаємо CORS middleware
-app.use(cors()); // Це дозволить усім доменам доступ до вашого API
+app.use(cors(allowedOrigins)); // Це дозволить усім доменам доступ до вашого API
 
 // Використовуємо body-parser для обробки JSON
 app.use(bodyParser.json()); // Для JSON в тілі запиту
 app.use(bodyParser.urlencoded({ extended: true })); // Для form-urlencoded
 
-// Стартуємо сервер
-app.post("/proxy", async (req, res) => {
-  const { email } = req.body;
-  if (!email) {
-    return res.status(400).json({ error: "Email is required" });
-  }
-  const apiUrl = `https://api.selzy.com/en/api/subscribe?format=json&api_key=6a7x15ac8s9qnekzwufrkq85m7gsbd1b94tu83uo&list_ids=25&fields[email]=${email}&tags=new_subscriber&double_optin=3&overwrite=0`;
-  try {
-    // Виконуємо запит до API за сформованим URL
-    const response = await axios.post(apiUrl);
-    // Відправляємо відповідь клієнту
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error while calling the external API:", error);
-    res.status(500).json({ error: "Failed to send data to the external API" });
-  }
-});
-
 app.post("/get-bonus", async (req, res) => {
   const { name } = req.body;
-console.log(req.body);
+// console.log(req.body);
 
   if (!name) {
     return res.status(400).json({ error: "Name and email are required" });
@@ -46,18 +48,41 @@ console.log(req.body);
   const existingBonus = await checkUserBonus(name);
   console.log(existingBonus);
   
-  if (existingBonus.bonusExist === "userExist_bonusExist"){
+  if (existingBonus.bonusExist === true){
     console.log("User already got bonus today", existingBonus.incomingUserDate);
     res.json({
-      getBonus: true,
+      bonusExist: true,
       dateOfBonus: existingBonus.incomingUserDate
     });
-  } else if (existingBonus.bonusExist === "userExist_bonusNotExist") {
+  } else if (existingBonus.bonusExist === false) {
     console.log("User doesn't have bonus today");
     res.json(await updateUserBonus(name, getRandomBonus(arr)));
   } else {
     console.log("User doesn't exist");
     res.json(await addNewUserBonus(name, getRandomBonus(arr)));
+  }
+});
+
+app.post("/check-bonus", async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: "Name and email are required" });
+  }
+
+
+  const existingBonus = await checkUserBonus(name);
+  console.log("existingBonus ----- ", existingBonus);
+  
+  if (existingBonus.bonusExist == true && existingBonus.bonusExist == true){
+    console.log("User already got bonus today", existingBonus.incomingUserDate);
+    res.json({existingBonus});
+  } else if (existingBonus.bonusExist == true && existingBonus.bonusExist == false) {
+    console.log("User doesn't have bonus today");
+    res.json(await updateUserBonus(name, getRandomBonus(arr)));
+  } else {
+    console.log("User doesn't exist");
+    res.json(existingBonus);
   }
 });
 

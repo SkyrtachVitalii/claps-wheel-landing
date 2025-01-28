c5e9878d19fc430dfbed2f05d76797b6b568d389
Vitalii Skyrtach <vitalijskirtac@gmail.com>
2025-01-28 16:09:12 +0200

    stable version before updating working with MongoDB

--- src/js/script.js
+++ src/js/script.js
@@ -1,163 +1,231 @@
-document.getElementById("enterEmail__btn").addEventListener("click", checkValidEmailAndcheckBonusExistence);
+document
+  .getElementById("enterEmail__btn")
+  .addEventListener("click", checkValidEmailAndcheckBonusExistence);
 const emailInput = document.getElementById("playerEmail");
-const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-let wheel = document.querySelector(".wheel__sectors");
-let wheelSpinBtn = document.querySelector(".wheel__btn");
-let buttonSpinBtn = document.querySelector(".btn__spinWheel");
+
+document.querySelector(".wheel__btn").addEventListener("click", mainSpinWheel);
+document.querySelector(".btn__spinWheel").addEventListener("click", mainSpinWheel);
 
 const sectors = [
-    { minDegree: 0, maxDegree: 22.5, value: "10 free spins to deposit", sectorDegree: 0 },
-    { minDegree: 22.6, maxDegree: 67.5, value: "120% to deposit", sectorDegree: 45 },
-    { minDegree: 67.6, maxDegree: 112.5, value: "10 free spins to deposit", sectorDegree: 90 },
-    { minDegree: 112.6, maxDegree: 157.5, value: "120% to deposit", sectorDegree: 135 },
-    { minDegree: 157.6, maxDegree: 202.5, value: "15 free spins to deposit", sectorDegree: 180 },
-    { minDegree: 202.6, maxDegree: 247.5, value: "120% to deposit", sectorDegree: 225 },
-    { minDegree: 247.6, maxDegree: 292.5, value: "No prize", sectorDegree: 270 },
-    { minDegree: 292.6, maxDegree: 337.5, value: "100% to deposit", sectorDegree: 315 },
-    { minDegree: 337.5, maxDegree: 360, value: "10 free spins to deposit", sectorDegree: 360 },
-]
+  {
+    minDegree: 0,
+    maxDegree: 22.5,
+    value: "10 free spins to deposit",
+    sectorDegree: 0,
+  },
+  {
+    minDegree: 22.6,
+    maxDegree: 67.5,
+    value: "120% to deposit",
+    sectorDegree: 45,
+  },
+  {
+    minDegree: 67.6,
+    maxDegree: 112.5,
+    value: "10 free spins to deposit",
+    sectorDegree: 90,
+  },
+  {
+    minDegree: 112.6,
+    maxDegree: 157.5,
+    value: "120% to deposit",
+    sectorDegree: 135,
+  },
+  {
+    minDegree: 157.6,
+    maxDegree: 202.5,
+    value: "15 free spins to deposit",
+    sectorDegree: 180,
+  },
+  {
+    minDegree: 202.6,
+    maxDegree: 247.5,
+    value: "120% to deposit",
+    sectorDegree: 225,
+  },
+  { minDegree: 247.6, maxDegree: 292.5, value: "No prize", sectorDegree: 270 },
+  {
+    minDegree: 292.6,
+    maxDegree: 337.5,
+    value: "100% to deposit",
+    sectorDegree: 315,
+  },
+  {
+    minDegree: 337.5,
+    maxDegree: 360,
+    value: "10 free spins to deposit",
+    sectorDegree: 360,
+  },
+];
 
-emailInput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        checkBonusExistence();
-    }
+emailInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    checkBonusExistence();
+  }
 });
 
 function blockwheelSpinBtn() {
-    wheelSpinBtn.style.pointerEvents = 'none';
-    buttonSpinBtn.style.pointerEvents = 'none';
-    wheelSpinBtn.removeEventListener("click", mainSpinWheel);
-    buttonSpinBtn.removeEventListener("click", mainSpinWheel);
+  document.querySelector(".wheel__btn").style.pointerEvents = "none";
+  document.querySelector(".btn__spinWheel").style.pointerEvents = "none";
+  document.querySelector(".wheel__btn").removeEventListener("click", mainSpinWheel);
+  document.querySelector(".btn__spinWheel").removeEventListener("click", mainSpinWheel);
 }
 
-function showBonus( bonusData) {
-    document.querySelector(".wheelContainer").style.display = "none";
-    bonusData.bonus === "No prize" ? showNoBonus() : showBonusAside(bonusData);
-
+function showBonus(bonusData) {
+  document.querySelector(".wheelContainer").style.display = "none";
+  bonusData.bonus === "No prize" ? showNoBonus() : showBonusAside(bonusData);
 }
 
-function showBonusAside(bonusData){
-    document.querySelector(".victory").style.display = "flex";
-    document.querySelector(".victory__prizeVal").innerText = bonusData.bonus;
+function showBonusAside(bonusData) {
+  document.querySelector(".victory").style.display = "flex";
+  document.querySelector(".victory__prizeVal").innerText = bonusData.bonus;
 }
-function showNoBonus(){
-    document.querySelector(".noPrize").style.display = "flex";
-    // document.querySelector(".victory__prizeVal").innerText = bonus;
+function showNoBonus() {
+  document.querySelector(".noPrize").style.display = "flex";
+  setInterval(updateCountdown, 1000);
 }
 
 async function getBonus(user) {
-    try {
-        const response = await fetch('http://localhost:3000/get-bonus', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        });
-        if (!response.ok) {
-            throw new Error('Запит не вдався');
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Помилка:', error);
+  try {
+    const response = await fetch("http://localhost:3000/get-bonus", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (!response.ok) {
+      throw new Error("Запит не вдався");
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Помилка:", error);
+  }
 }
 
 function defineBonusSectorDegree(bonus) {
-    for (const sector of sectors) {
-        if (sector.value === bonus) {
-            return -sector.sectorDegree;
-        }
+  for (const sector of sectors) {
+    if (sector.value === bonus) {
+      return -sector.sectorDegree;
     }
+  }
 }
 
 function spinWheel(rotationDegree) {
-    wheel.style.transition = "transform 4s ease-in-out";
-    wheel.style.transform = "translate(-50%, -50%) rotate(" + rotationDegree + "deg)";
+  let wheel = document.querySelector(".wheel__sectors");
+  wheel.style.transition = "transform 4s ease-in-out";
+  wheel.style.transform =
+    "translate(-50%, -50%) rotate(" + rotationDegree + "deg)";
 }
 
-let finalRotation = 0;
-let prevDegree = 0;
+function mainSpinWheel() {
+  const user = {
+    name: document.getElementById("playerEmail").value,
+  };
 
-wheelSpinBtn.addEventListener("click", mainSpinWheel);
-buttonSpinBtn.addEventListener("click", mainSpinWheel);
+  getBonus(user).then((bonusData) => {
+    const customWheelRotation = 5;
+    blockwheelSpinBtn();
 
-function mainSpinWheel () {
-    console.log("mainSpinWheel");
-    
-    const user = {
-        name: document.getElementById("playerEmail").value
-    };
-    console.log(user);
-    
+    const randomRotation = 360 * customWheelRotation;
+    const bonusSectorDegree = defineBonusSectorDegree(bonusData.bonus);
 
-    getBonus(user).then(bonusData => {
-        const customWheelRotation = 5;
-        blockwheelSpinBtn();
+    if (bonusSectorDegree !== null) {
+      let finalRotation = 0;
+      finalRotation += randomRotation + bonusSectorDegree;
+      spinWheel(finalRotation);
+      setTimeout(() => {
+        showBonus(bonusData);
+      }, 5000);
+    }
+  });
+}
 
-        const randomRotation = 360 * customWheelRotation;
-        const bonusSectorDegree = defineBonusSectorDegree(bonusData.bonus);
+document.querySelectorAll(".containerCloseButton").forEach((button) => {
+  button.addEventListener("click", closeBtn);
+});
 
-        if (bonusSectorDegree !== null) {
-            finalRotation += randomRotation + bonusSectorDegree;
-            spinWheel(finalRotation);
-            setTimeout(() => {
-                showBonus(bonusData);
-            }, 5000);
-        }
-    });
-};
-
-document.querySelectorAll(".containerCloseButton").forEach(button => {
-    button.addEventListener("click", closeBtn);
-})
-
-function closeBtn(){
-    this.closest('aside').style.display = "none";
+function closeBtn() {
+  this.closest("aside").style.display = "none";
 }
 
 function getEmail() {
-    const email = document.getElementById("playerEmail").value;
-    return email;
+  const email = document.getElementById("playerEmail").value;
+  return email;
 }
 
-function checkValidEmailAndcheckBonusExistence(){
-    const email = getEmail();
-    if (email === "") {
-        alert("Please, enter your email");
-        return;
+async function checkValidEmailAndcheckBonusExistence() {
+  const email = getEmail();
+  if (email === "") {
+    alert("Please, enter your email");
+    return;
+  } else {
+    if (!isEmailValid(email)) {
+      alert("Please, enter valid email");
     } else {
-        // isEmailValid(email) ? document.querySelector(".wheelContainer").style.display = "flex" : alert("Please, enter valid email");
-        if (!isEmailValid(email)){
-            alert("Please, enter valid email");
-        } else {
-            document.querySelector(".wheelContainer").style.display = "flex";
-        }
+      const bonusExistance = await checkBonusExistence(email);
+      if (bonusExistance.userExist == false) {
+        document.querySelector(".wheelContainer").style.display = "flex";
+      } else if (bonusExistance.bonusExist == false) {
+        document.querySelector(".wheelContainer").style.display = "flex";
+      } else {
+        document.querySelector(".bonusReceived").style.display = "flex";
+        setInterval(updateCountdown, 1000);
+      }
     }
+  }
 }
 
 function isEmailValid(value) {
-    return EMAIL_REGEXP.test(value);
+  const EMAIL_REGEXP =/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+  return EMAIL_REGEXP.test(value);
 }
 
-async function checkBonusExistence(email){
-    try {
-        const response = await fetch('http://localhost:3000/get-bonus', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        });
-        if (!response.ok) {
-            throw new Error('Запит не вдався');
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Помилка:', error);
+async function checkBonusExistence(email) {
+  const user = {
+    name: email,
+  };
+  try {
+    const response = await fetch("http://localhost:3000/check-bonus", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (!response.ok) {
+      throw new Error("Запит не вдався");
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Помилка:", error);
+  }
 }
 
+function updateCountdown() {
+  const now = new Date(); // Поточна дата та час
+  const midnight = new Date(now); // Створюємо нову дату для опівночі
+  midnight.setHours(24, 0, 0, 0); // Встановлюємо час на опівночі
+
+  const diff = midnight - now; // Різниця в мілісекундах
+
+  if (diff <= 0) {
+    document.getElementById("countdown").textContent = "Опівніч вже настала!";
+    return;
+  }
+
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document
+    .querySelectorAll(".btn__timer")
+    .forEach(
+      (element) => (element.textContent = `${hours}:${minutes}:${seconds}`)
+    );
+
+  console.log(`${hours}:${minutes}:${seconds}`);
+}
